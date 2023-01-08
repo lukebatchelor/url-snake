03dee184c5b6bf3bd943519c7b2e603847f0b56c
Luke.Batchelor <batchelor.luke+git@gmail.com>
2023-01-08 18:40:35 +0800

    Adds footer to built dists with minified code and links

--- run.js
+++ run.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
+const path = require('path');
 const UglifyJS = require("uglify-js");
 
 const DEBUG = !!process.env.DEBUG;
+const DIST = process.env.DIST;
+
 const devFileStr = fs.readFileSync("./dev.html", "utf-8");
 const [html, js] = devFileStr
   .split("<script>")
@@ -24,6 +27,9 @@ let minifyOpts = {
     drop_console: true,
     unsafe: true,
   },
+  output: {
+    quote_style: 3
+  }
 };
 const readableMinified = UglifyJS.minify(js, minifyOpts).code;
 minifyOpts.mangle.toplevel = true;
@@ -48,7 +54,24 @@ if (DEBUG) {
   });
 }
 
-const indexFileStr = html + '<script>' + minified + '</script>';
-fs.writeFileSync('./index.html', indexFileStr)
-
 console.log(url);
+
+// Write output files
+
+const fileStr = html + '<script>' + minified + '</script>';
+// if we aren't build the distribution builds, just output the index.html file in current working directory
+if (!DIST) {
+  fs.writeFileSync('./index.html', fileStr)
+  process.exit(0);
+}
+
+// otherwise, we'll build the distribution version of the file and write it to the dist/ dir
+const outFile = path.join(__dirname, 'dist', DIST+'.html');
+const footerJsPreTag = `<p>Minified javascript code (${minified.length} bytes)</p><pre id=minified></pre>`;
+const footerUrlPreTag = `<p>Encoded data url (${url.length} bytes)</p><pre id=url></pre>`;
+const footerAnchorsTags = `<p><a href="https://url-snake.netlify.app/">Mobile friendly version</a> &#183; <a href="https://url-snake.netlify.app/golf.html">Code golf'd version</a> &#183; <a href="https://github.com/lukebatchelor/url-snake/tree/main">Github</a></p>`;
+const footerScriptsAndStyles = `<script>minified.innerText = '${minified}';url.innerText='${url}'</script><style>pre{max-width: 60vmin;white-space:pre-line;word-break:break-all;background:#999;padding:1em;}style,script{display: none;}</style>`
+const footerStr = footerJsPreTag + footerUrlPreTag + footerAnchorsTags + footerScriptsAndStyles;
+
+fs.writeFileSync(outFile, fileStr + footerStr);
+
